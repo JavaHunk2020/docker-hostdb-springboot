cd9652a1ca2e38eefb34d162818eccdc98d21d8e
Nagendra Kumar <nagendra.kumar@controlmap.io>
2023-04-08 04:32:32 +0530

    database outside docker container

---
+++ src/main/java/net/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
@@ -0,0 +1,18 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author LENOVO
+ *
+ */
+@SpringBootApplication
+public class SpringbootRestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/net/javaguides/springboot/entity/User.java
@@ -0,0 +1,32 @@
+package net.javaguides.springboot.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+}
